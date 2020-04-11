e9842db1a36e0511ae200d149516385a281da440
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2020-04-11 20:18:44 +0200

    cmake: remove dynamic-list linker option
    dynamic-list (exported_symbols_list on Mac) was used to forbid
    export of all symbols of the tarantool executable except a given
    list. Motivation of that was to avoid hacking the linker with
    false usage of symbols needed to be exported. As a consequence,
    symbols not listed in these options became invisible.
    
    Before these options, when a symbol was defined, but not used in
    the final executable, the linker could throw it away, even though
    many symbols were used by Lua FFI, or should be visible for user's
    dynamic modules. Where the linker, obviously, can't see if they
    are needed.
    
    To make the linker believe the symbols are actually needed there
    was a hack with getting pointers at these functions and doing
    something with them.
    
    For example, assume we have 'test()' function in 'box' static
    library:
    
        int
        test(void);
    
    It is not used anywhere in the final executable. So to trick the
    linker there is a function 'export()' declared, which takes a
    pointer at 'test()' and seemingly does something with it (or
    actually does - it does not matter):
    
        void
        export()
        {
            void *syms[] = {test};
            if (time(NULL) == 0) {
                syms[0]();
                syms[1]();
                ...
            }
        }
    
    Some users want to use not documented but visible symbols, so the
    patch removes the dynamic-list option, and returns the linker
    hack back, but in a more structured form.
    
    Part of #2971

---
+++ src/exports.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010-2020, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#define EXPORT(func) extern void ** func(void)
+
+/**
+ * The file is a hack to force the linker keep the needed symbols
+ * in the result tarantool executable file.
+ *
+ * Problem is that if a symbol is defined but never used, the
+ * linker may throw it away. But many symbols are needed for Lua
+ * FFI and for the public C API used by dynamic modules.
+ *
+ * This file creates a 'false usage' of needed symbols. It takes
+ * pointers at them and does something with them, so as the
+ * compiler and linker couldn't remove it.
+ *
+ * Some exporters may represent modules having submodules, and may
+ * aggregate symbols from them.
+ *
+ * Add new exporters here. Keep them in alphabetical order.
+ */
+
+void
+export_syms(void)
+{
+	void *syms[] = {
+	};
+	const int func_count = sizeof(syms) / sizeof(syms[0]);
+	for (int i = 0; i < func_count; ++i)
+		((void **(*)(void))syms[i])();
+}
+
+#undef EXPORT
